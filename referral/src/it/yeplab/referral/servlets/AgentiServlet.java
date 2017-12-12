9ca979d63aea1d04bd7280cd64111bf6e73459e1
ciccio@CM15 <ciccio.milea@gmail.com>
2017-12-12 12:34:27 +0100

    Signed-off-by: ciccio@CM15 <ciccio.milea@gmail.com>

--- referral/src/it/yeplab/referral/servlets/AgentiServlet.java
+++ referral/src/it/yeplab/referral/servlets/AgentiServlet.java
@@ -26,18 +26,19 @@ public class AgentiServlet extends HttpServlet {
 		String msg = "";
 		String fun = req.getParameter("fun");
 		if (fun.equals("nuovo")) {
-			String denominazione=req.getParameter("denominazione");
-			String referente=req.getParameter("referente");
-			String indirizzo=req.getParameter("indirizzo");
-			String telefono=req.getParameter("telefono");
-			String email=req.getParameter("email");
-			String cf=req.getParameter("cf");
-			String piva=req.getParameter("piva");
-			String mezzopagamento=req.getParameter("mezzopagamento");
-			String coordinatepagamento=req.getParameter("coordinatepagamento");
-			ManagerAgenti ma=new ManagerAgenti(DBConnectionFactory.getConnection());
-			int id=ma.addAgente(denominazione, referente, indirizzo, telefono, email, cf, piva, mezzopagamento, coordinatepagamento);
-			msg="nuovo agente = "+id;
+			String denominazione = req.getParameter("denominazione");
+			String referente = req.getParameter("referente");
+			String indirizzo = req.getParameter("indirizzo");
+			String telefono = req.getParameter("telefono");
+			String email = req.getParameter("email");
+			String cf = req.getParameter("cf");
+			String piva = req.getParameter("piva");
+			String mezzopagamento = req.getParameter("mezzopagamento");
+			String coordinatepagamento = req.getParameter("coordinatepagamento");
+			ManagerAgenti ma = new ManagerAgenti(DBConnectionFactory.getConnection());
+			int id = ma.addAgente(denominazione, referente, indirizzo, telefono, email, cf, piva, mezzopagamento,
+					coordinatepagamento);
+			msg = "nuovo agente = " + id;
 		}
 		if (fun.equals("cancella")) {
 
